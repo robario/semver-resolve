60c1374c750476fb6ca98e16e198f396610185d7
robario <webmaster@robario.com>
2016-09-13 17:12:18 +0900

    :recycle: Refactor to lint pass

--- index.js
+++ index.js
@@ -2,23 +2,13 @@
 // node_modules
 var cartesianProduct = require('cartesian-product');
 var semver = require('semver');
+// local
+var debug = require('./lib/debug.js');
 
 // In order to accurately understand the variable name is, please check https://github.com/npm/node-semver#range-grammar
 
 var MINIMUM_RANGE = '>=0.0.0';
 var MAXIMUM_RANGE = '<=' + [Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER].join('.');
-
-var debug;
-if (typeof process === 'object' && typeof process.env === 'object' && /\bsemver[-_]?resolve\b/i.test(process.env.NODE_DEBUG)) {
-    debug = function () {
-        var args = Array.prototype.slice.call(arguments);
-        args.unshift('SEMVER-RESOLVE');
-        console.log.apply(console, args);
-    };
-} else {
-    debug = function () {
-    };
-}
 
 function nullFilter(value) {
     return value !== null;
@@ -26,55 +16,6 @@ function nullFilter(value) {
 
 function uniqueFilter(value, index, array) {
     return array.indexOf(value) === index;
-}
-
-// return resolved intersection of range-set list
-function semverResolve(args, errorCallback) {
-    debug('input:', args);
-    errorCallback = errorCallback || function () {};
-
-    var rangeLists = args.map(function (rangeSet) {
-        return rangeSet
-            .split(/\s*[|]{2}\s*/)
-            .map(function (range) {
-                // make a safe for semver.validRange
-                var test = {
-                    range: range.replace(/[*]/g, MINIMUM_RANGE).replace(/\blatest\b/g, MAXIMUM_RANGE),
-                    type: 'valid',
-                    version: null,
-                };
-                var validRange = semver.validRange(test.range);
-                var log = 'range = ' + JSON.stringify(range) + ':';
-                if (validRange) {
-                    debug(log + ' success ' + JSON.stringify(validRange));
-                    return validRange;
-                } else {
-                    debug(log + ' failure');
-                    errorCallback(test);
-                    return null;
-                }
-            })
-            .filter(nullFilter);
-    });
-    var resolvedRangeSet = cartesianProduct(rangeLists)
-            .map(function (rangeList, index) {
-                var range = rangeList.join(' ');
-                var result = resolveRange(range);
-                var log = 'rangeSetsList[' + index + '] = ' + JSON.stringify(range) + ':';
-                if (result.constructor === String) {
-                    debug(log + ' success ' + JSON.stringify(result));
-                    return result;
-                } else {
-                    debug(log + ' failure ' + JSON.stringify(result));
-                    errorCallback(result);
-                    return null;
-                }
-            })
-            .filter(nullFilter)
-            .filter(uniqueFilter)
-            .join(' || ');
-    debug('output:', resolvedRangeSet);
-    return resolvedRangeSet;
 }
 
 function resolveRange(range) {
@@ -99,8 +40,8 @@ function resolveRange(range) {
         var op = 0 < exp.length ? exp.pop() : '';
         if (op[0] === '>') {
             if (!min ||
-                op === '>' && semver.gt(version, min.version) ||
-                op === '>=' && semver.gte(version, min.version)) {
+                (op === '>' && semver.gt(version, min.version)) ||
+                (op === '>=' && semver.gte(version, min.version))) {
                 min = {
                     op: op,
                     version: version,
@@ -108,8 +49,8 @@ function resolveRange(range) {
             }
         } else if (op[0] === '<') {
             if (!max ||
-                op === '<' && semver.lt(version, max.version) ||
-                op === '<=' && semver.lte(version, max.version)) {
+                (op === '<' && semver.lt(version, max.version)) ||
+                (op === '<=' && semver.lte(version, max.version))) {
                 max = {
                     op: op,
                     version: version,
@@ -171,6 +112,53 @@ function resolveRange(range) {
     }
 
     return resolvedRange.replace(MINIMUM_RANGE, '*').replace(MAXIMUM_RANGE, 'latest');
+}
+
+// return resolved intersection of range-set list
+function semverResolve(args, errorCallback) {
+    debug('input:', args);
+    errorCallback = errorCallback || function () { /* do nothing. */ };
+
+    var rangeLists = args.map(function (rangeSet) {
+        return rangeSet
+            .split(/\s*[|]{2}\s*/)
+            .map(function (range) {
+                // make a safe for semver.validRange
+                var test = {
+                    range: range.replace(/[*]/g, MINIMUM_RANGE).replace(/\blatest\b/g, MAXIMUM_RANGE),
+                    type: 'valid',
+                    version: null,
+                };
+                var validRange = semver.validRange(test.range);
+                var log = 'range = ' + JSON.stringify(range) + ':';
+                if (!validRange) {
+                    debug(log + ' failure');
+                    errorCallback(test);
+                    return null;
+                }
+                debug(log + ' success ' + JSON.stringify(validRange));
+                return validRange;
+            })
+            .filter(nullFilter);
+    });
+    var resolvedRangeSet = cartesianProduct(rangeLists)
+            .map(function (rangeList, index) {
+                var range = rangeList.join(' ');
+                var result = resolveRange(range);
+                var log = 'rangeSetsList[' + index + '] = ' + JSON.stringify(range) + ':';
+                if (result.constructor !== String) {
+                    debug(log + ' failure ' + JSON.stringify(result));
+                    errorCallback(result);
+                    return null;
+                }
+                debug(log + ' success ' + JSON.stringify(result));
+                return result;
+            })
+            .filter(nullFilter)
+            .filter(uniqueFilter)
+            .join(' || ');
+    debug('output:', resolvedRangeSet);
+    return resolvedRangeSet;
 }
 
 module.exports = semverResolve;
